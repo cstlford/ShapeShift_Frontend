0f7c86eac6bb801ae6aa18677e3c22492c4abd47
nhuffman-3rdeyecam <nhuffman@doveresg.com>
2024-09-19 17:55:59 -0400

    add FormComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import FormComponent from "./FormComponent/FormComponent";
+
 function App() {
   return (
     <>
       <h1>Welcome to ShapeShift</h1>
       <p>Let's get started...</p>
+      <FormComponent />
     </>
   );
 }
