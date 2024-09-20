4f4a15a5b8ef58b004b3925a7226768e8d108084
Austin <austin_sarwar@yahoo.com>
2024-09-19 22:40:14 -0400

    Create User Preferences From

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import UserPreferencesForm from './components/UserPreferencesForm';
+
 function App() {
   return (
     <>
-      <h1>Welcome to ShapeShift</h1>
-      <p>Let's get started...</p>
+    
+      <UserPreferencesForm/>
     </>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // This will make Vite use polling. This is so live updates work for wsl.
+    },
+    port: 5173, // Optional: Change the port if needed
+  },
 })
