c79629c5dcc7621d2f05694ea5d3e8d18a3957f6
nhuffman-3rdeyecam <nhuffman@doveresg.com>
2024-09-20 11:10:42 -0400

    define global colors

--- frontend/src/FormComponent/FormComponent.tsx
+++ frontend/src/FormComponent/FormComponent.tsx
@@ -18,49 +18,52 @@ const FormComponent = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log('Form Data:', formData);
-    // You can handle form submission here (e.g., send data to an API)
+    // handle form submission here (e.g., send data to an API)
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          Age:
-          <input
-            type="number"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Height (cm):
-          <input
-            type="number"
-            name="height"
-            value={formData.height}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Weight (kg):
-          <input
-            type="number"
-            name="weight"
-            value={formData.weight}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="userForm">
+        <form onSubmit={handleSubmit}>
+        <div>
+            <label>
+            Age:
+            <input
+                type="number"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
+                required
+            />
+            </label>
+        </div>
+        <div>
+            <label>
+            Height (cm):
+            <input
+                type="number"
+                name="height"
+                value={formData.height}
+                onChange={handleChange}
+                required
+            />
+            </label>
+        </div>
+        <div>
+            <label>
+            Weight (kg):
+            <input
+                type="number"
+                name="weight"
+                value={formData.weight}
+                onChange={handleChange}
+                required
+            />
+            </label>
+        </div>
+        <button type="submit">Submit</button>
+        </form>
+    </div>
+
   );
 };
 
