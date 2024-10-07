8170687c1ff35d64472e7057f5def7554fc955e6
nhuffman-3rdeyecam <nhuffman@doveresg.com>
2024-10-07 10:42:59 -0400

    update sidenav in dashboard

--- frontend/src/components/MainComponents/SideNavbarComponent/index.tsx
+++ frontend/src/components/MainComponents/SideNavbarComponent/index.tsx
@@ -4,15 +4,35 @@ import "./index.css";
 const SideNavbarComponent = () => {
   return (
     <aside className="sidenav-container">
+      <div className="sidenav-header">
+        <h1>Dashboard</h1>
+      </div>
       <ul className="sidenav-list">
+        <h2>Nutrition</h2>
         <li className="sidenav-item">
-          <NavLink to="/dashboard">item1</NavLink>
+          <NavLink to="/dashboard">Make a Plan</NavLink>
         </li>
         <li className="sidenav-item">
-          <NavLink to="/dashboard">item2</NavLink>
+          <NavLink to="/dashboard">My Recipes</NavLink>
         </li>
         <li className="sidenav-item">
-          <NavLink to="/dashboard">item3</NavLink>
+          <NavLink to="/dashboard">Food Logging</NavLink>
+        </li>
+
+        <h2>Exercise</h2>
+        <li className="sidenav-item">
+          <NavLink to="/dashboard">Make a Plan</NavLink>
+        </li>
+        <li className="sidenav-item">
+          <NavLink to="/dashboard">Exercise How-To Guide</NavLink>
+        </li>
+        <li className="sidenav-item">
+          <NavLink to="/dashboard">Exercise Logging</NavLink>
+        </li>
+
+        <h2>Progress Tracking</h2>
+        <li className="sidenav-item">
+          <NavLink to="/dashboard">Weight Logging</NavLink>
         </li>
       </ul>
     </aside>
