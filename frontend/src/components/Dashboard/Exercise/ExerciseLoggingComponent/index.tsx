a6859b8e338a8556ef43101ec6eaec776d4d2c74
nhuffman-3rdeyecam <nhuffman@doveresg.com>
2024-10-08 10:23:47 -0400

    components for sidenav items

---
+++ frontend/src/components/Dashboard/Exercise/ExerciseLoggingComponent/index.tsx
@@ -0,0 +1,22 @@
+import NavbarComponent from "../../NavbarComponent";
+import SideNavbarComponent from "../../SideNavbarComponent";
+import "./index.css";
+import "../../../index.css";
+
+const ExcerciseLoggingComponent: React.FC = () => {
+  return (
+    <div className="dashboard-container">
+      <NavbarComponent />
+      <SideNavbarComponent />
+      <div className="content-container">
+        <div className="box box1">Content 1</div>
+        <div className="box-container">
+          <div className="box box2">Content 2</div>
+          <div className="box box3">Content 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExcerciseLoggingComponent;
