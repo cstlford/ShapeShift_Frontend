67cba0837cbc543b263bc0e17e5f470e0804b785
nhuffman-3rdeyecam <nhuffman@doveresg.com>
2024-10-08 10:25:02 -0400

    ..

--- frontend/src/components/Dashboard/Progress/ProgressLoggingComponent/index.tsx
+++ frontend/src/components/Dashboard/Progress/ProgressLoggingComponent/index.tsx
@@ -9,10 +9,10 @@ const ProgressLoggingComponent: React.FC = () => {
       <NavbarComponent />
       <SideNavbarComponent />
       <div className="content-container">
-        <div className="box box1">Content 1</div>
+        <div className="box box1">Progress 1</div>
         <div className="box-container">
-          <div className="box box2">Content 2</div>
-          <div className="box box3">Content 3</div>
+          <div className="box box2">Progress 2</div>
+          <div className="box box3">Progress 3</div>
         </div>
       </div>
     </div>
